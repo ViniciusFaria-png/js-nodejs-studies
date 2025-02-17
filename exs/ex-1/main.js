cad61b3675c0d576d6faccc1a7ee18671588bb4c
Vinicius Faria <vinicius.f4ria@gmail.com>
2025-02-17 11:22:53 -0300

    ex-1-alura

---
+++ exs/ex-1/main.js
@@ -0,0 +1,44 @@
+let name = "Vinicius";
+const age = 24;
+var isMan = true;
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isMan)
+
+let firstname = "Vinicius";
+let lastName = "Faria";
+
+let fullName = firstname + " " + lastName;
+
+console.log(fullName);
+
+let herName = "Joana";
+let herAge = 54;
+
+console.log(`${herName} tem ${herAge} anos`)
+
+herAge = 55;
+
+console.log(`${herName} tem ${herAge} anos`)
+
+var hisAge = 34
+let myAge = 24
+if(1 > 0){
+    var itsAge = 22
+    console.log(hisAge);
+    console.log(myAge);
+    console.log(itsAge);
+}
+
+console.log(hisAge);
+console.log(myAge);
+console.log(itsAge);
+
+let isRaining = false;
+
+if(isRaining == true){
+    console.log("Esta chovendo, leve um guarda-chuva");
+} else {
+    console.log("Nao esta chovendo, nao precisa levar um guarda-chuva")
+}
